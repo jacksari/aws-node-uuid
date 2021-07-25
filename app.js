8ca43efe53e3d3fb766d0773532c336cf05f0f78
Jack Sari <janasarii@gmail.com>
2021-07-24 19:07:19 -0500

    rrr

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ const {v4: uuidv4} = require('uuid');
 const cors = require('cors');
 const fs = require('fs')
 
-const httpsServerOptions = {
-    key: fs.readFileSync(process.env.KEY_PATH),
-    cert: fs.readFileSync(process.env.CERT_PATH)
-}
+//const httpsServerOptions = {
+//    key: fs.readFileSync(process.env.KEY_PATH),
+//    cert: fs.readFileSync(process.env.CERT_PATH)
+//}
 
 // Esto es un comentario de prueba
 const app = express();
@@ -23,13 +23,13 @@ app.use(cors());
 // Servidor HTTP
 const serverHttp = http.createServer(app);
 serverHttp.listen(process.env.HTTP_PORT, process.env.IP);
-const serverHttps = https.createServer(httpsServerOptions, app);
-serverHttps.listen(process.env.HTTPS_PORT, process.env.IP)
+//const serverHttps = https.createServer(httpsServerOptions, app);
+//serverHttps.listen(process.env.HTTPS_PORT, process.env.IP)
 
-app.use((req, res, next) => {
-    if(req.secure) next();
-    else res.redirect(`https://${req.headers.host}${req.url}`);
-})
+//app.use((req, res, next) => {
+//    if(req.secure) next();
+//    else res.redirect(`https://${req.headers.host}${req.url}`);
+//})
 
 
 // Contenido estático
